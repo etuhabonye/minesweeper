b5c0f1da37749b25a3cdd019e3f4c7e222a0b7fb
Emma Tuhabonye <70925748+etuhabonye@users.noreply.github.com>
2022-05-11 14:13:18 -0400

    precision recall and f1 score added

--- MineSweeper/naiveBayes.py
+++ MineSweeper/naiveBayes.py
@@ -1,5 +1,12 @@
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression 
+from sklearn.metrics import roc_curve, auc
+import pandas as pd 
+import matplotlib.pyplot as plt # for data visualization purposes
+import seaborn as sns # for statistical data visualization
+
 import generateminesweeper
 
 target = "Safe"
@@ -31,7 +38,58 @@ y_pred = classifer.predict(X)
 # df2 = generateminesweeper.boardTodf()
 
 X2 = df2[features]
-y_pred2 = classifer.predict(X2)
-df2["Predicted"] = y_pred2
+y_true = df2[target]
+y_pred = classifer.predict(X2)
+df2["Predicted"] = y_pred
 
-print(df2)
+print(accuracy_score(y_true, y_pred))
+
+from sklearn.metrics import confusion_matrix
+
+cm = confusion_matrix(y_true, y_pred)
+
+print('Confusion matrix\n\n', cm)
+
+print('\nTrue Positives(TP) = ', cm[0,0])
+
+print('\nTrue Negatives(TN) = ', cm[1,1])
+
+print('\nFalse Positives(FP) = ', cm[0,1])
+
+print('\nFalse Negatives(FN) = ', cm[1,0])
+
+# visualize confusion matrix with seaborn heatmap
+
+cm_matrix = pd.DataFrame(data=cm, columns=['Actual Positive:1', 'Actual Negative:0'], 
+                                 index=['Predict Positive:1', 'Predict Negative:0'])
+
+sns.heatmap(cm_matrix, annot=True, fmt='d', cmap='YlGnBu')
+
+TP = cm[0,0]
+TN = cm[1,1]
+FP = cm[0,1]
+FN = cm[1,0]
+
+from sklearn.metrics import classification_report
+
+print(classification_report(y_true, y_pred))
+
+# print classification accuracy
+
+classification_accuracy = (TP + TN) / float(TP + TN + FP + FN)
+
+print('Classification accuracy : {0:0.4f}'.format(classification_accuracy))
+
+# print precision score
+
+precision = TP / float(TP + FP)
+
+
+print('Precision : {0:0.4f}'.format(precision))
+
+#print recall or sensitivity
+
+recall = TP / float(TP + FN)
+
+print('Recall or Sensitivity : {0:0.4f}'.format(recall))
+
